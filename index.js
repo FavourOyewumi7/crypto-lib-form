eb2a263f6dc5652bc0a91db0022a71150ad14479
FavourOyewumi7 <oyewumifavour65@gmail.com>
2022-07-07 14:27:57 +0100

    done with the base structure just a little bit of input and i'm done

--- index.js
+++ index.js
@@ -17,11 +17,40 @@ async function postData(url = '', data = {}) {
   };
 
 let button = document.getElementById('button');
+
+
+let names =  document.getElementById('name').value;
+let title = document.getElementById('title').value;
+let resource = document.getElementById('resource').value;
+let description = document.getElementById('description').value;
+let link = document.getElementById('link').value;
+
+data = {
+    'name': names,
+    'title': title,
+    'resource': resource,
+    'description': description,
+    'link': link
+
+}
+
+let print =()=>{
+    console.log('omo')
+    console.log(data);
+}
+
+
 //Name
  // Title
 // Resource
 // Description
 // Link
 
+/*let inputs = [...document.querySelectorAll('input')];
+let names =  inputs[0].value;
+let email =inputs[1].value;
+let resource = inputs[2].value;
+let description =  inputs[3].value;
+let link = inputs[4].value;*/
 
-button.onclick(postData())
+
